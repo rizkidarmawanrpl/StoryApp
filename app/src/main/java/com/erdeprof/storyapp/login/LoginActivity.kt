5f7867aea2d2b1ad9e74bc955d9ba1052332907b
Rizki Darmawan <rizkidarmawanrpl@gmail.com>
2022-11-10 09:44:50 +0700

    Commit update animasi login

--- app/src/main/java/com/erdeprof/storyapp/login/LoginActivity.kt
+++ app/src/main/java/com/erdeprof/storyapp/login/LoginActivity.kt
@@ -3,12 +3,17 @@ package com.erdeprof.storyapp.login
 // import com.erdeprof.storyapp.login.data.Staff
 // import org.jetbrains.anko.startActivity
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.system.Os.uname
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.erdeprof.storyapp.R
@@ -43,6 +48,8 @@ class LoginActivity : AppCompatActivity(), LoginView {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        playAnimation()
     }
 
     override fun onSuccessLogin(msg: String?, data: User?) {
@@ -74,5 +81,35 @@ class LoginActivity : AppCompatActivity(), LoginView {
         }.show()*/
 
         Toast.makeText(this@LoginActivity, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun playAnimation() {
+        val ivGambar = findViewById<ImageView>(R.id.ivGambar)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val loginEmail = findViewById<EditText>(R.id.loginEmail)
+        val loginPassword = findViewById<EditText>(R.id.loginPassword)
+        val tvTitle = findViewById<TextView>(R.id.tvTitle)
+
+        ObjectAnimator.ofFloat(ivGambar, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val login = ObjectAnimator.ofFloat(btnLogin, View.ALPHA, 1f).setDuration(500)
+        val register = ObjectAnimator.ofFloat(btnRegister, View.ALPHA, 1f).setDuration(500)
+        val email = ObjectAnimator.ofFloat(loginEmail, View.ALPHA, 1f).setDuration(500)
+        val password = ObjectAnimator.ofFloat(loginPassword, View.ALPHA, 1f).setDuration(500)
+        val title = ObjectAnimator.ofFloat(tvTitle, View.ALPHA, 1f).setDuration(500)
+
+        val together = AnimatorSet().apply {
+            playTogether(login, register)
+        }
+
+        AnimatorSet().apply {
+            playSequentially(title, email, password, together)
+            start()
+        }
     }
 }
